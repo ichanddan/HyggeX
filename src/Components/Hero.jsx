f3ecbf2d7ce4548dbba8a90359e677e1f7a253ee
ichanddan <code.us.cm@gmail.com>
2024-04-26 18:29:19 +0530

    Responsive Hero Section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,11 +6,11 @@ import { IoReload } from "react-icons/io5";
 
 export const Hero = () => {
   return (
-    <div className="px-44">
+    <div className="px-4 md:px-40">
       <div>
-        <Breadcrumbs className="text-[18px] mt-5">
+        <Breadcrumbs className="text-[5px] mt-5 md:text-[18px]">
           <BreadcrumbItem>
-            <GoHome className="text-2xl" />
+            <GoHome className="text-xl md:text-2xl" />
           </BreadcrumbItem>
           <BreadcrumbItem>Flashcard</BreadcrumbItem>
           <BreadcrumbItem>Mathematics</BreadcrumbItem>
@@ -18,10 +18,10 @@ export const Hero = () => {
         </Breadcrumbs>
       </div>
       <div className="mt-7">
-        <h1 className="text-[32px] @apply text-[#121481] @apply font-[700px]">
-          Relations and Functions ( Mathematics )
+        <h1 className="text-[25px] @apply text-[#121481] @apply font-[700px] md:text-[32px]">
+          Relations and Functions <br className="md:hidden" /> ( Mathematics )
         </h1>
-        <div className="flex items-center justify-center gap-9 mt-7 @apply w-[Hug_(546px)px] text-[20px]">
+        <div className="flex items-center justify-center gap-5 mt-7  text-xl md:gap-9 @apply w-[Hug_(546px)px] text-[20px]">
           <p className="text-[#121481] @apply border-b-2 border-b-[#121481] border-solid">
             Study
           </p>
@@ -31,28 +31,28 @@ export const Hero = () => {
           <p>Others</p>
         </div>
         <div className="flex items-center justify-center mt-7">
-          <div className="@apply w-[650px] h-[350.19px] bg-[#121481] rounded-3xl">
-            <h1 className="grid place-items-center m-44 text-white text-4xl">
+          <div className="lg:@apply w-[650px] h-[350.19px] bg-[#121481] rounded-3xl">
+            <h1 className="text-center mt-40 md: text-white text-4xl">
               9 + 6 + 7x - 2x - 3
             </h1>
           </div>
         </div>
         <div className="flex items-center justify-center flex-col mt-3">
-          <div className="flex items-center justify-around @apply w-[500px] text-[#121481]">
+          <div className="@apply w-[21rem] flex items-center justify-around text-[#121481] md:@apply w-[500px]">
             <IoReload className="text-3xl" />
-            <IoIosArrowBack className="p-1 bg-[#121481] text-white text-4xl rounded-full" />
-            <p className="text-2xl">01/10</p>
-            <IoIosArrowForward className="p-1 bg-[#121481] text-white text-4xl rounded-full" />
+            <IoIosArrowBack className="p-1 bg-[#121481] text-white text-3xl rounded-full md:text-4xl" />
+            <p className="text-xl md:text-2xl">01/10</p>
+            <IoIosArrowForward className="p-1 bg-[#121481] text-white text-3xl rounded-full md:text-4xl" />
             <GoScreenFull className="text-3xl" />
           </div>
         </div>
-        <div className="flex items-center justify-between">
+        <div className="flex items-center gap-10 md:justify-between my-5 ">
           <div>
-            <img src="/logo-1.png" width={200} alt="" />
+            <img src="/logo-1.png" className="w-36 md:w-[200px]" alt="" />
           </div>
           <div className="flex items-center justify-center gap-2">
-            <IoIosAdd className="text-4xl bg-[#121481] text-white rounded-full" />
-            <p className="text-xl text-[#121481]">Create Flashcard</p>
+            <IoIosAdd className="text-2xl md:text-4xl bg-[#121481] text-white rounded-full" />
+            <p className="text-sm md:text-xl text-[#121481]">Create Flashcard</p>
           </div>
         </div>
       </div>
