4337d37edfb3e840c8bb940abb2793064ba3f8ad
ichanddan <code.us.cm@gmail.com>
2024-04-26 19:03:06 +0530

    update favicon

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Navbars } from './Components/Navbar'
 import { Hero } from './Components/Hero'
 import { FAQ } from './Components/FAQ'
-
+document.title="Hyggex"
 export default function App() {
   return (
     <div>
