fa23cfdc116219f19d413252b7a43113ac448544
ichanddan <code.us.cm@gmail.com>
2024-04-26 15:38:32 +0530

    Complete fqa

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Navbar } from './Components/Navbar'
 import { Hero } from './Components/Hero'
+import { FAQ } from './Components/FAQ'
 
 export default function App() {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <FAQ/>
     </div>
   )
 }
---
+++ src/Components/FAQ.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {Accordion, AccordionItem} from "@nextui-org/react";
+
+
+export const FAQ = () => {
+    const defaultContent =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  return (
+    <div className="px-44">
+      <h1 className="text-4xl text-[#121481] @apply font-bold tracking-[0.005em]">
+        FAQ
+      </h1>
+      <div className="py-5 mb-5">
+        <Accordion>
+          <AccordionItem key="1" aria-label="Accordion 1" title="Can education flashcards be used for all age groups?">
+            {defaultContent}
+          </AccordionItem>
+          <AccordionItem key="2" aria-label="Accordion 2" title="How do education flashcards work?">
+            {defaultContent}
+          </AccordionItem>
+          <AccordionItem key="3" aria-label="Accordion 3" title="Can education flashcards be used for test preparation?">
+            {defaultContent}
+          </AccordionItem>
+        </Accordion>
+      </div>
+    </div>
+  );
+};
