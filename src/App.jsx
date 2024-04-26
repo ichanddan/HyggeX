c3b960df1ad74190c86b7a477c3161e8ee1dc650
ichanddan <code.us.cm@gmail.com>
2024-04-26 17:03:52 +0530

    Responsive NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Navbar } from './Components/Navbar'
+import { Navbars } from './Components/Navbar'
 import { Hero } from './Components/Hero'
 import { FAQ } from './Components/FAQ'
 
 export default function App() {
   return (
     <div>
-      <Navbar/>
+      <Navbars/>
       <Hero/>
       <FAQ/>
     </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,22 +1,86 @@
-import { Link } from '@nextui-org/react'
+import { Button, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from '@nextui-org/react';
 import React from 'react'
 
-export const Navbar = () => {
+export const Navbars = () => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    "Home",
+    "Flashcard",
+    "Contect",
+    "FQA",
+  ];
   return (
-    <div>
-        <div className='flex items-center justify-around p-3'>
-            <div>
-                <img src="/Logo.png" width={120} alt="" />
-            </div>
-            <div className='flex items-center text-black gap-7'>
-                <a href='#'>Home</a>
-                <a href='#'>Flashcard</a>
-                <a href='#'>Contact</a>
-                <a href='#'>Contact</a>
-                <a href='#'>FAQ</a>
-                <a href='#' className='@apply bg-[linear-gradient(180deg,#06286E_0%,#164EC0_100%)] rounded-3xl px-5 py-2 text-white'>Login</a>
-            </div>
+    <Navbar
+    isBordered
+    isMenuOpen={isMenuOpen}
+    onMenuOpenChange={setIsMenuOpen}
+    >
+      <NavbarContent className="sm:hidden" justify="start">
+        <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden pr-3" justify="center">
+        <NavbarBrand>
+          <img src="/Logo.png" width={150} />
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarBrand>
+          <img src="/Logo.png" width={150} />
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <div className="hidden sm:flex gap-7">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Home
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Flashcard
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Contect
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            FQA
+          </Link>
+        </NavbarItem>
+
         </div>
-    </div>
+        <NavbarItem>
+          <Button as={Link} href="#" className='@apply bg-[linear-gradient(180deg,#06286E_0%,#164EC0_100%)] rounded-3xl px-5 py-2 text-white'>
+            Login
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+
+
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className="w-full"
+              color={
+                index === 2 ? "warning" : index === menuItems.length - 1 ? "danger" : "foreground"
+              }
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
   )
 }
+
+{/* <a href='#' className='@apply bg-[linear-gradient(180deg,#06286E_0%,#164EC0_100%)] rounded-3xl px-5 py-2 text-white'>Login</a> */}
